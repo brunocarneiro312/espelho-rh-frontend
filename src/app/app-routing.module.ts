5b48fe64a1f4b25397aeed920b0914463f12c482
Bruno Carneiro <bruno.souza@grancursosonline.com.br>
2020-09-24 19:12:23 -0300

    signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {SignInFormComponent} from './sign-in-form/sign-in-form.component';
 import {HomeComponent} from './home/home.component';
 import {SignUpFormComponent} from './sign-up-form/sign-up-form.component';
 import {UserComponent} from './user/user.component';
+import {AuthGuard} from './auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'signin', component: SignInFormComponent },
   { path: 'signup', component: SignUpFormComponent },
-  { path: 'users', component: UserComponent },
-  { path: '**', redirectTo: 'signin' }
+  { path: 'users', component: UserComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
